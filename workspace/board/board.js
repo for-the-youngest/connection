3e66b5b977a842b2f03fb8d3305937c940cb3473
byungwook <byungwook.dev@gmail.com>
2024-04-10 18:25:54 +0900

    240410 메인페이지, 회원정보 수정페이지 완성입니다

--- workspace/board/board.js
+++ workspace/board/board.js
@@ -1,14 +1,43 @@
-$(".menu").on("click", checkMenuOn);
+$(".header-menu").on("click", checkMenuOn);
 
-let $communityHiddenMenu = $(".communityHiddenMenu");
+let $headerCommunityHiddenMenu = $(".header-communityHiddenMenu");
 
 function checkMenuOn() {
   // 현재 display 속성을 가져와서 "none"인지 확인
-  if ($communityHiddenMenu.css("display") === "none") {
-    $communityHiddenMenu.css("display", "block");
+  if ($headerCommunityHiddenMenu.css("display") === "none") {
+    $headerCommunityHiddenMenu.css("display", "block");
   } else {
-    $communityHiddenMenu.css("display", "none");
+    $headerCommunityHiddenMenu.css("display", "none");
   }
 };
+
+
+$(document).ready(function() {
+  $('.main-pageNumberNum1').click(function() {
+    var currentColor = $(this).css('color');
+    if (currentColor === 'rgb(80, 121, 226)') {
+      $(this).css('color', '#a5a5a5');
+    } else {
+      $(this).css('color', '#5079E2');
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('.main-pageNumberNum').click(function() {
+    var currentColor = $(this).css('color');
+    if (currentColor === 'rgb(80, 121, 226)') {
+      $(this).css('color', '#a5a5a5');
+    } else {
+      $(this).css('color', '#5079E2');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $(".main-downscrollButton").click(function(){
+    $(".main-sortOrder").toggle();
+  });
+});
 
 
