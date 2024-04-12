49a125f06e53298e4deb3fa491508f919f2d82af
byungwook <byungwook.dev@gmail.com>
2024-04-12 20:08:32 +0900

    240412 fetch적용입니다

--- workspace/info-edit/info-edit.js
+++ workspace/info-edit/info-edit.js
@@ -6,20 +6,24 @@ $(document).ready(function () {
 
   //년도 selectbox 만들기
   for (i = 1900; i <= year; i++) {
-    $('.infoEdit-birthYear').append('<option value="' + i + '">' + i + "년" + '</option>');
+    $(".infoEdit-birthYear").append(
+      '<option value="' + i + '">' + i + "년" + "</option>"
+    );
   }
 
   //월 selectbox 만들기
   for (i = 1; i <= 12; i++) {
-    $('.infoEdit-birthMonth').append('<option value="' + i + '">' + i + "월" + '</option>');
+    $(".infoEdit-birthMonth").append(
+      '<option value="' + i + '">' + i + "월" + "</option>"
+    );
   }
-
 
   //일 selectbox 만들기
   for (i = 1; i <= 31; i++) {
-    $('.infoEdit-birthDay').append('<option value="' + i + '">' + i + "일" + '</option>');
+    $(".infoEdit-birthDay").append(
+      '<option value="' + i + '">' + i + "일" + "</option>"
+    );
   }
-
 });
 
 // 성별 라디오 버튼
@@ -28,81 +32,109 @@ $(document).ready(function () {
 const radios = document.querySelectorAll('input[type="radio"]');
 
 // 라디오 버튼에 이벤트 리스너 추가
-radios.forEach(radio => {
-  radio.addEventListener('click', () => {
+radios.forEach((radio) => {
+  radio.addEventListener("click", () => {
     // 현재 라디오 버튼이 체크되어 있는지 확인
     const isChecked = radio.checked;
 
     // 모든 라디오 버튼의 체크 상태 초기화
-    radios.forEach(otherRadio => {
-        if (otherRadio !== radio) {
-            otherRadio.checked = false;
-        }
+    radios.forEach((otherRadio) => {
+      if (otherRadio !== radio) {
+        otherRadio.checked = false;
+      }
     });
 
     // 체크된 경우에만 체크 상태 유지
     if (isChecked) {
-        radio.checked = true;
+      radio.checked = true;
     }
-
   });
 });
 
 // 이름, 닉네임 유효성 검사
 
 function validateName() {
-  const inputName = document.getElementById('infoEdit-userName').value;
+  const inputName = document.getElementById("infoEdit-userName").value;
 
   // 이름의 길이가 2자 이상 10자 이하인지 확인
   if (inputName.length < 2 || inputName.length > 10) {
-      document.getElementById('infoEdit-errMsgName').textContent = '2자 이상 10자 이하로 입력해야 합니다';
-      document.getElementById('infoEdit-errMsgName').style.display = 'block';
-      return false;
+    document.getElementById("infoEdit-errMsgName").textContent =
+      "2자 이상 10자 이하로 입력해야 합니다";
+    document.getElementById("infoEdit-errMsgName").style.display = "block";
+    return false;
   }
 
   // 이름에 한글 또는 영어가 포함되어 있는지 확인
   const regex = /^[가-힣a-zA-Z]+$/;
   if (!regex.test(inputName)) {
-      document.getElementById('infoEdit-errMsgName').textContent = '이름은 한글 또는 영어만 포함해야 합니다';
-      document.getElementById('infoEdit-errMsgName').style.display = 'block';
-      return false;
+    document.getElementById("infoEdit-errMsgName").textContent =
+      "이름은 한글 또는 영어만 포함해야 합니다";
+    document.getElementById("infoEdit-errMsgName").style.display = "block";
+    return false;
   }
 
-  document.getElementById('infoEdit-errMsgName').textContent = ''; // 오류 메시지 초기화
+  document.getElementById("infoEdit-errMsgName").textContent = ""; // 오류 메시지 초기화
   return true;
-
 }
 
-function validateNickName(){
-  const inputNickName = document.getElementById('infoEdit-userNickName').value;
+function validateNickName() {
+  const inputNickName = document.getElementById("infoEdit-userNickName").value;
 
   // 2자 이상 10자 이하, 공백 없이 특수문자 '_'만 사용 가능
 
   // 2자 이상 10자 이하인지 확인
-  if(inputNickName.length < 2 || inputNickName.length > 10) {
-    document.getElementById('infoEdit-errorMsgNkName').textContent = '2자 이상 10자 이하로 입력해야 합니다';
-    document.getElementById('infoEdit-errorMsgNkName').style.display = 'block';
+  if (inputNickName.length < 2 || inputNickName.length > 10) {
+    document.getElementById("infoEdit-errorMsgNkName").textContent =
+      "2자 이상 10자 이하로 입력해야 합니다";
+    document.getElementById("infoEdit-errorMsgNkName").style.display = "block";
     return false;
   }
-  
+
   // 공백 확인
-  if(inputNickName.includes(" ")) {
-    document.getElementById('infoEdit-errorMsgNkName').textContent = '공백이 없어야 합니다';
-    document.getElementById('infoEdit-errorMsgNkName').style.display = 'block';
+  if (inputNickName.includes(" ")) {
+    document.getElementById("infoEdit-errorMsgNkName").textContent =
+      "공백이 없어야 합니다";
+    document.getElementById("infoEdit-errorMsgNkName").style.display = "block";
     return false;
   }
-  
+
   // 특수문자 확인
   const Regex = /^[가-힣a-zA-Z_]+$/;
-  if(!Regex.test(inputNickName)) {
-    document.getElementById('infoEdit-errorMsgNkName').textContent = '공백 없이 특수문자 "_"만 사용 가능합니다';
-    document.getElementById('infoEdit-errorMsgNkName').style.display = 'block';
+  if (!Regex.test(inputNickName)) {
+    document.getElementById("infoEdit-errorMsgNkName").textContent =
+      '공백 없이 특수문자 "_"만 사용 가능합니다';
+    document.getElementById("infoEdit-errorMsgNkName").style.display = "block";
     return false;
   }
 
-  document.getElementById('infoEdit-errorMsgNkName').textContent = ''; // 오류 메시지 초기화
+  document.getElementById("infoEdit-errorMsgNkName").textContent = ""; // 오류 메시지 초기화
   return true;
-
 }
 
+// 띄어쓰기 2칸 입력필드
 
+// 이름
+const inputNameField = document.getElementById("infoEdit-userName");
+
+inputNameField.addEventListener("keydown", function (event) {
+  // 입력 필드의 값
+  let value = this.value;
+
+  // 띄어쓰기를 2칸으로 추가
+  if (event.key !== "Backspace" && value.length === 0) {
+    this.value = "  "; // 띄어쓰기 2칸
+  }
+});
+
+// 닉네임
+const inputNickNameField = document.getElementById("infoEdit-userNickName");
+
+inputNickNameField.addEventListener("keydown", function (event) {
+  // 입력 필드의 값
+  let value = this.value;
+
+  // 띄어쓰기를 2칸으로 추가
+  if (event.key !== "Backspace" && value.length === 0) {
+    this.value = "  "; // 띄어쓰기 2칸
+  }
+});
--- workspace/main/main.js
+++ workspace/main/main.js
@@ -14,60 +14,57 @@ console.log(`slideCnt : ${slideCnt}`);
 
 $(".main-nextBtn-slide").on("click", moveNext);
 function moveNext() {
-    console.log(`슬라이드 다음 클릭`);
-    currentIdx++;
-    console.log(`currentIdx : ${currentIdx}`);
-    
-    $bannerBox.css("left", -(currentIdx * bannerWidth));
-    $bannerBox.css("transition", "0.5s ease");
-    checkEnd();
+  console.log(`슬라이드 다음 클릭`);
+  currentIdx++;
+  console.log(`currentIdx : ${currentIdx}`);
+
+  $bannerBox.css("left", -(currentIdx * bannerWidth));
+  $bannerBox.css("transition", "0.5s ease");
+  checkEnd();
 }
 
-
 $(".main-prevBtn-slide").on("click", function () {
-    console.log(`슬라이드 이전 클릭`);
-    currentIdx--;
-    console.log(`currentIdx : ${currentIdx}`);
+  console.log(`슬라이드 이전 클릭`);
+  currentIdx--;
+  console.log(`currentIdx : ${currentIdx}`);
 
-    $bannerBox.css("left", -(currentIdx * bannerWidth));
-    $bannerBox.css("transition", "0.5s ease");
-    checkEnd();
+  $bannerBox.css("left", -(currentIdx * bannerWidth));
+  $bannerBox.css("transition", "0.5s ease");
+  checkEnd();
 });
 
 checkEnd();
 
 //처음 이미지와 마지막 이미지는 화살표 감추기
 function checkEnd() {
-    if (currentIdx <= 0) {
-        $(".main-prevBtn-slide").css("display", "none");
-    } else {
-        $(".main-prevBtn-slide").css("display", "block");
-    }
+  if (currentIdx <= 0) {
+    $(".main-prevBtn-slide").css("display", "none");
+  } else {
+    $(".main-prevBtn-slide").css("display", "block");
+  }
 
-    if (currentIdx >= slideCnt - 1) {
-        $(".main-nextBtn-slide").css("display", "none");
-    } else {
-        $(".main-nextBtn-slide").css("display", "block");
-    }
+  if (currentIdx >= slideCnt - 1) {
+    $(".main-nextBtn-slide").css("display", "none");
+  } else {
+    $(".main-nextBtn-slide").css("display", "block");
+  }
 }
 
 //fetch를 사용하여 헤더와 푸터를 가져오고 삽입하는 함수
 function fetchAndInsert() {
-    // 헤더를 가져옴
-    fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-    })
-    
+  // 헤더를 가져옴
+  fetch("../header/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
 
-    // 푸터를 가져옴
-    fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer').innerHTML = data;
-    })
-    
+  // 푸터를 가져옴
+  fetch("../footer/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
 }
 
 // 페이지 로드 시 fetchAndInsert 함수 실행
