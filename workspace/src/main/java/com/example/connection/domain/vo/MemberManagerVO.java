8da585a6f3f8e004be17d2572fe0ef388bb05bc9
gyulban <park55533@naver.com>
2024-11-17 15:26:52 +0900

    20241117 기존 데이터 삭제

--- workspace/src/main/java/com/example/connection/domain/vo/MemberManagerVO.java
+++
@@ -1,9 +0,0 @@
-package com.example.connection.domain.vo;
-
-import lombok.Data;
-
-@Data
-public class MemberManagerVO {
-    private long memberNumber;
-    private long managerNumber;
-}
--- workspace/src/main/resources/static/js/board-create.js
+++
@@ -1,7 +0,0 @@
-// 파일 삭제
-const inputFile = document.querySelector("input[type='file']");
-
-function deleteFile(e) {
-  console.log(inputFile);
-  inputFile.value = '';
-}
